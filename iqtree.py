c0f99371ab6129771bd0c6fc4a063371839adb34
flashhhhh <nguyenchithanhpro196@gmail.com>
2023-11-30 22:58:15 +0700

    change to draw 10 lines

--- iqtree.py
+++ iqtree.py
@@ -2,57 +2,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-argv1 = sys.argv[1]
-argv2 = sys.argv[2]
+argv = sys.argv[1]
+masterColor = ['', 'red', 'orange', 'violet', 'pink', 'purple']
+mpiColor = ['', 'blue', 'green', 'black', 'brown', 'gray']
 
-# Load data
-with open(argv1) as f:
-    data = f.readlines()
-    data = [x.strip() for x in data]
 
-    for i in range (0, len(data)):
-        data[i] = data[i].split()
-    
-    data = np.array(data)
-    data[:, 0] = data[:, 0].astype(int)
-    data[:, 1] = data[:, 1].astype(float)
-    
-    X = int(data[0, 0])
-    y = float(data[0, 1])
+for suffix in range(1, 6):
+    with open("master/" + argv + "/" + argv + "_" + str(suffix) + ".data") as f:
+        data = f.readlines()
+        data = [x.strip() for x in data]
 
-    for i in range(1, len(data)):
-        X = np.append(X, int(data[i, 0]) - 1)
-        y = np.append(y, float(data[i - 1, 1]))
+        for i in range (0, len(data)):
+            data[i] = data[i].split()
         
-        X = np.append(X, int(data[i, 0]))
-        y = np.append(y, float(data[i, 1]))
-
-    # Plot data
-    plt.plot(X, y, color='red')
-
-
-with open(argv2) as f:
-    data = f.readlines()
-    data = [x.strip() for x in data]
-
-    for i in range (0, len(data)):
-        data[i] = data[i].split()
-    
-    data = np.array(data)
-    data[:, 0] = data[:, 0].astype(int)
-    data[:, 1] = data[:, 1].astype(float)
-    
-    X = int(data[0, 0])
-    y = float(data[0, 1])
-
-    for i in range(1, len(data)):
-        X = np.append(X, int(data[i, 0]) - 1)
-        y = np.append(y, float(data[i - 1, 1]))
+        data = np.array(data)
+        data[:, 0] = data[:, 0].astype(int)
+        data[:, 1] = data[:, 1].astype(float)
         
-        X = np.append(X, int(data[i, 0]))
-        y = np.append(y, float(data[i, 1]))
+        X = int(data[0, 0])
+        y = float(data[0, 1])
 
-    # Plot data
-    plt.plot(X, y, color='blue')
+        for i in range(1, len(data)):
+            X = np.append(X, int(data[i, 0]) - 1)
+            y = np.append(y, float(data[i - 1, 1]))
+            
+            X = np.append(X, int(data[i, 0]))
+            y = np.append(y, float(data[i, 1]))
+
+        # Plot data
+        plt.plot(X, y, color=masterColor[suffix])
+
+
+for suffix in range(1, 6):
+    with open("minhmpi/" + argv + '/' + argv + '_' + str(suffix) + '.data') as f:
+        data = f.readlines()
+        data = [x.strip() for x in data]
+
+        for i in range (0, len(data)):
+            data[i] = data[i].split()
+        
+        data = np.array(data)
+        data[:, 0] = data[:, 0].astype(int)
+        data[:, 1] = data[:, 1].astype(float)
+        
+        X = int(data[0, 0])
+        y = float(data[0, 1])
+
+        for i in range(1, len(data)):
+            X = np.append(X, int(data[i, 0]) - 1)
+            y = np.append(y, float(data[i - 1, 1]))
+            
+            X = np.append(X, int(data[i, 0]))
+            y = np.append(y, float(data[i, 1]))
+
+        # Plot data
+        plt.plot(X, y, color=mpiColor[suffix])
 
 plt.show()
+
+"""
+Command is:
+    python3 iqtree.py <name of file>
+    
+For example:
+    python3 iqtree.py M7929
+"""
