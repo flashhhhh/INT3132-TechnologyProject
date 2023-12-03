ab3d01072ad130df1f75191001c7289eae18ce0c
danquan <dangquanattvhouse@gmail.com>
2023-12-03 13:22:10 +0700

    hoangmpi added partially

--- iqtree.py
+++ iqtree.py
@@ -8,6 +8,8 @@ masterColor = ['', 'red', 'red', 'red', 'red', 'red']
 #mpiColor = ['', 'blue', 'green', 'black', 'brown', 'gray']
 mpiColor = ['', 'blue', 'blue', 'blue', 'blue', 'blue']
 
+hoangColor = ['', 'green', 'green', 'green', 'green', 'green']
+
 for suffix in range(1, 6):
     with open("master/" + argv + "/" + argv + "_" + str(suffix) + ".data") as f:
         data = f.readlines()
@@ -57,6 +59,32 @@ for suffix in range(1, 6):
 
         # Plot data
         plt.plot(X, y, color=mpiColor[suffix])
+
+
+for suffix in range(1, 4):
+    with open("hoangmpi/" + argv + '/' + argv + '_' + str(suffix) + '.data') as f:
+        data = f.readlines()
+        data = [x.strip() for x in data]
+
+        for i in range (0, len(data)):
+            data[i] = data[i].split()
+        
+        data = np.array(data)
+        data[:, 0] = data[:, 0].astype(int)
+        data[:, 1] = data[:, 1].astype(float)
+        
+        X = int(data[0, 0])
+        y = float(data[0, 1])
+
+        for i in range(1, len(data)):
+            X = np.append(X, int(data[i, 0]) - 1)
+            y = np.append(y, float(data[i - 1, 1]))
+            
+            X = np.append(X, int(data[i, 0]))
+            y = np.append(y, float(data[i, 1]))
+
+        # Plot data
+        plt.plot(X, y, color=hoangColor[suffix])
 
 plt.savefig("graph/" + argv + ".png")
 # plt.show()
